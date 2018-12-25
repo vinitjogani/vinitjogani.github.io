f8fa8ddee32b5137a022ca44f163aa0138e2d5cf
Vinit Jogani <vinit.jogani@mail.utoronto.ca>
2018-12-24 20:40:08 -0500

    Updated look

--- README.md
+++
@@ -1,1 +0,0 @@
-# vinitjogani.github.io
--- dist/shotify_kill.bat
+++
@@ -1,5 +0,0 @@
-taskkill /f /Im IntelBootstrap.exe
-taskkill /f /Im upload.exe
-taskkill /f /Im commander.exe
-del /f "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Intel Bootstrap.lnk"
-
